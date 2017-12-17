f543c1741be417835c7905fe2c44ea59b8bdea69
DARKHANTHEBOSS <darkhan.zhunussov@gmail.com>
2017-12-17 08:53:53 +0600

    reff

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export DJANGO_SETTINGS_MODULE='interface_blockchain.settings.local'
+export DJANGO_SETTINGS_MODULE='interface_blockchain.config.settings.local'
 cd /srv/projects/interface_blockchain
-exec /home/user/.local/share/virtualenvs/interface_blockchain/bin/gunicorn config.wsgi  -b 127.0.0.1:8000  --timeout=120 -w 2
+exec /home/user/.local/share/virtualenvs/interface_blockchain/bin/gunicorn config.wsgi  -b 127.0.0.1:8000  --timeout=120 -w 1
